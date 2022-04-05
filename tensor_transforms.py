51d5e80a2666e2bd727120922e4f79d56380ba69
twice154 <terraada@naver.com>
2022-04-05 02:19:40 +0000

    submited

--- tensor_transforms.py
+++ tensor_transforms.py
@@ -8,8 +8,10 @@ def convert_to_coord_format(b, h, w, device='cpu', integer_values=False):
         x_channel = torch.arange(w, dtype=torch.float, device=device).view(1, 1, 1, -1).repeat(b, 1, w, 1)
         y_channel = torch.arange(h, dtype=torch.float, device=device).view(1, 1, -1, 1).repeat(b, 1, 1, h)
     else:
-        x_channel = torch.linspace(-1, 1, w, device=device).view(1, 1, 1, -1).repeat(b, 1, w, 1)
-        y_channel = torch.linspace(-1, 1, h, device=device).view(1, 1, -1, 1).repeat(b, 1, 1, h)
+        x_channel = torch.linspace(-1.5, 1.5, int(1.5*w), device=device).view(1, 1, 1, -1).repeat(b, 1, h, 1)
+        y_channel = torch.linspace(-1, 1, h, device=device).view(1, 1, -1, 1).repeat(b, 1, 1, int(1.5*w))
+        # x_channel = torch.linspace(-1, 1, w, device=device).view(1, 1, 1, -1).repeat(b, 1, w, 1)
+        # y_channel = torch.linspace(-1, 1, h, device=device).view(1, 1, -1, 1).repeat(b, 1, 1, h)
     return torch.cat((x_channel, y_channel), dim=1)
 
 
